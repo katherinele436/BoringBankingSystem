64c8069f904ce372612b4d2263d321b26f6238e3
Katherine Le <14ttul@queensu.ca>
2017-10-17 21:16:51 -0400

    move everything to main

--- src/com/company/BoringBankingSystem.java
+++ src/com/company/BoringBankingSystem.java
@@ -1,7 +1,15 @@
 package com.company;
-//import com.company.Session;
+import com.company.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class BoringBankingSystem {
+
+
+
     public static int[] readValidAccounts(){
         int[] accountsList = null;
         return accountsList;
@@ -25,8 +33,58 @@ public class BoringBankingSystem {
 
     }
 
+    public static String createAccount() throws IOException {
+        if (!mode){
+            int accNum = getInt("Enter new account number: ");
+            if (!validAccNum(accNum)){//if account is not in Valid Account List
+                String accName = getStringInput("Enter new account name: ");
+                if (validAccName(accName)){
+                    System.out.println("account " + accNum + " is created for " + accName);
+                    return "NEW" + accNum + "000 0000000" + accName;
+                }
+            }
+        }
+        return "";
 
-    public static void Main(String[] args) {
+    }
+
+    public static String deleteAccount() throws IOException {
+        if (!mode){
+            int accNum = getInt("enter account number: ");
+            if (validAccNum(accNum)) { //if Account is in valid account list
+                System.out.println("account" + accNum + " is deleted");
+            }
+        }
+        return "";
+    }
+
+    public static boolean validAccNum(int accNum){ //used to test if account number is correctly format and if it already exists  - Back End ?
+        return true;
+    }
+
+    public static boolean validAccName(String accName) { //used to test if account name is valid - Back End ?
+        return true;
+    }
+
+
+    public static String getStringInput ( String prompt) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String accNumStr;
+        accNumStr = br.readLine();
+        System.out.println(prompt);
+        return accNumStr;
+    }
+
+    public static int getInt( String prompt) throws IOException {
+        try {
+            return Integer.parseInt(getStringInput(prompt));
+        }
+        catch (NumberFormatException e){
+        }
+        return  0;
+    }
+
+    public static void Main(String[] args) throws IOException {
         // write your code here
         boolean login = false;
         int[] validAccountsList = readValidAccounts();
