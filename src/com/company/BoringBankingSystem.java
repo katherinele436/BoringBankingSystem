7f4500d6e207bdcb2329539eef774106fb6aa63d
Michael Kerr <mikekerr247@hotmail.com>
2017-10-19 20:43:21 -0400

    fixed readInput issues

--- src/com/company/BoringBankingSystem.java
+++ src/com/company/BoringBankingSystem.java
@@ -1,18 +1,11 @@
 package com.company;
 
-import com.company.Session;
-
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 public class BoringBankingSystem {
     public static Session Account = null;
     public static ArrayList<Integer> validAccountsList = null;
-    public static ArrayList<String> summaryString = null;
     public static String summaryFile;
     public static String accountsListFile;
 
@@ -24,40 +17,162 @@ public class BoringBankingSystem {
         while ((line = readList.readLine()) != null && !line.equals("0000000")) {
             accountsList.add(Integer.parseInt(line));
         }
-
         return accountsList;
     }
 
     public static Session waitForLogin(boolean login) throws IOException {
         String loginStr = getStringInput("");
         if (loginStr.equals("login")){
-            String modeStr = getStringInput("login as agent or machine?");
-            if (modeStr.equals("agent")){ Account.mode = true;}
-            else if(modeStr.equals("machine")){ Account.mode = false;}
+            String modeStr = getStringInput("log in as agent or machine?");
+            if (modeStr.equals("agent")){
+                System.out.println("logged in as agent");
+                Account.mode = true;
+            }
+            else if(modeStr.equals("machine")){
+                System.out.println("logged in as machine");
+                Account.mode = false;
+            }
         }
         else if(loginStr.equals("quit")){
             System.exit(0);
         }
         System.out.println("error: invalid login");
-        return waitForLogin(login);
+        return null;
     }
 
-    public static String readNextInput(){
-        String input = null;
-        if (inputFile != null)
+    public static void createAccount() throws IOException {
+        if (!Account.mode){
+            int accNum = getInt("Enter new account number: ");
+            //if account is not in Valid Account List
+            if (!validAccNum(accNum) && !validAccList(accNum)){
+                String accName = getStringInput("Enter new account name: ");
+                if (validAccName(accName)){
+                    System.out.println("account " + accNum + " is created for " + accName);
+                    // append summary string to summary list
+                    Account.summary.add("NEW" + accNum + "000 0000000" + accName);
+                }
+            }
+        }
+        else {
+            System.out.println("error: cannot create account in machine mode, transaction ended");
+        }
+    }
+
+    public static void deleteAccount() throws IOException {
+        if (!Account.mode) {
+            int accNum = getInt("enter account number: ");
+            //if Account is in valid account list
+            if (validAccList(accNum)) {
+                System.out.println("account" + accNum + " is deleted");
+                // append summary string summary list
+                Account.summary.add("DEL" + accNum + "000 0000000 ***");
+            }
+        }
+        else {
+            System.out.println("error: cannot delete account in machine mode, transaction ended");
+        }
+    }
+
+    public static void deposit() throws IOException{
+        int input = getInt("enter account number:");
+        int account_number;
+        if (validAccList((input))){
+            account_number = input;
+        }
+        else{
+            System.out.println("error: account does not exist, transaction ended");
+            return;
+        }
+        int amount;
         try{
-            FileReader fileReader = new FileReader(inputFile);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            input = bufferedReader.readLine();
-            bufferedReader.close();
-            return input;
-        }catch(FileNotFoundException e){
-            System.out.println("Could not find valid accounts list file");
+            input = getInt("enter amount");
+        }catch (NumberFormatException e){
+            System.out.println("error: invalid amount, transaction ended");
+            return;
         }
-        catch(IOException ex){
-            System.out.println("Error reading valid accounts list file");
+        amount = input;
+        double dollars;
+        if (withinSingleDepositLimit(amount)){
+            dollars = amount / 100;
+        }else{
+            System.out.println("error: single deposit limit exceeded");
+            return;
         }
-        return input;
+        System.out.printf("Deposited $%0.2f into account %d", dollars, account_number);
+        Account.summary.add("DEP 0000000 " + amount + " " + account_number + " ***\n");
+    }
+
+    public static void withdraw()throws IOException{
+        int input = getInt("enter account number:");
+        int account_number;
+        if (validAccList(input)){
+            account_number = input;
+        }
+        else{
+            System.out.println("error: account does not exist, transaction ended");
+            return;
+        }
+        int amount;
+        try{
+            input = getInt("enter amount:");
+            amount = input;
+        }catch (NumberFormatException e){
+            System.out.println("error: invalid amount, transaction ended");
+            return;
+        }
+        double dollars;
+        if (withinSingleWithdrawLimit(amount)){
+            if (withinTotalWithdrawLimit(amount)) {
+                dollars = amount / 100;
+            }
+            else{
+                System.out.println("error: total withdraw limit exceeded");
+                return;
+            }
+        }else{
+            System.out.println("error: single withdraw limit exceeded");
+            return;
+        }
+        System.out.printf("Withdrew $%0.2f from account %d", dollars, account_number);
+        Account.summary.add("WDR 0000000 " + amount + " " + account_number + " ***\n");
+    }
+
+    public static void transfer() throws IOException{
+        int input = getInt("transfer from account number:");
+        int account_one, account_two;
+        if (validAccList(input)){
+            account_one = input;
+        }
+        else{
+            System.out.println("error: account does not exist, transaction ended");
+            return;
+        }
+        input = getInt("transfer to account number:");
+        if (validAccList(input)){
+            account_two = input;
+        }
+        else{
+            System.out.println("error: account does not exist, transaction ended");
+            return;
+        }
+        int amount;
+        try{
+            input = getInt("enter amount:");
+            amount = input;
+        }catch (NumberFormatException e){
+            System.out.println("error: invalid amount, transaction ended");
+            return;
+        }
+        double dollars;
+        if (withinSingleTransferLimit(amount)){
+            dollars = amount / 100;
+        }
+        else{
+            System.out.println("error: single transfer limit exceeded");
+            return;
+        }
+        System.out.printf("Transferred $%0.2f from account %d to account %d", dollars, account_one, account_two);
+        Account.summary.add("XFR " + account_one + " " + amount + " " + account_two + " ***\n");
     }
 
     public static boolean logout(boolean login){
@@ -65,17 +180,15 @@ public class BoringBankingSystem {
             Account.summary.add("EOS *** *** *** ***\n");
             return true;
         }else{
-            System.out.println("erorr: cannot logout if not logged in");
+            System.out.println("error: cannot logout if not logged in");
             return false;
         }
     }
 
     public static void writeSummaryFile() throws IOException{
-
         try {
-            FileWriter writer = new FileWriter("output.txt");
+            FileWriter writer = new FileWriter(summaryFile);
             for (String str : Account.summary) {
-
                 writer.write(str);
             }
             writer.close();
@@ -84,72 +197,75 @@ public class BoringBankingSystem {
             //summary file error message
         }
     }
-    public static void deposit(){
-        System.out.println("enter account number:");
-        String input = readNextInput();
-        String account_number = null;
 
-        if (validAccList(Integer.parseInt(input))){
-            account_number = input;
-        }
-        else{
-            System.out.println("error: account does not exist, transaction ended");
-            return;
-        }
-
-        System.out.println("enter amount:");
-        input = readNextInput();
-        int amount = 0;
-        try{
-            amount = Integer.parseInt(input);
-        }catch (NumberFormatException e){
-            System.out.println("error: invalid amount, transaction ended");
-            return;
-        }
-
-        double dollars = 0;
-        if (withinSingleDepositLimit(amount)){
-                dollars = amount / 100;
-        }else{
-            System.out.println("error: single deposit limit exceeded");
-            return;
-        }
-
-        System.out.printf("Deposited $%0.2f into account %d", dollars, account_number);
-        Account.summary.add("DEP 0000000 " + amount + " " + account_number + " ***\n");
-
+    public static String getStringInput ( String prompt) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String accNumStr;
+        System.out.println(prompt);
+        accNumStr = br.readLine();
+        return accNumStr;
     }
 
-    public static void createAccount() throws IOException {
-        if (!Account.mode){
-            int accNum = getInt("Enter new account number: ");
-            if (!validAccNum(accNum) && !validAccList(accNum)){//if account is not in Valid Account List
-                String accName = getStringInput("Enter new account name: ");
-                if (validAccName(accName)){
-                    System.out.println("account " + accNum + " is created for " + accName);
-                    Account.summary.add("NEW" + accNum + "000 0000000" + accName);// append summary string to summaryString arraylist
-                }
-            }
+    public static int getInt( String prompt) throws IOException {
+        try {
+            return Integer.parseInt(getStringInput(prompt));
         }
-        else { System.out.println("error: cannot create account in machine mode, transaction ended"); }
-    }
-
-    public static void deleteAccount() throws IOException {
-        if (!Account.mode) {
-            int accNum = getInt("enter account number: ");
-            if (validAccList(accNum)) { //if Account is in valid account list
-                System.out.println("account" + accNum + " is deleted");
-                Account.summary.add("DEL" + accNum + "000 0000000 ***");// append summary string to summaryString arraylist
-            }
+        catch (NumberFormatException e){
         }
-        else { System.out.println("error: cannot delete account in machine mode, transaction ended"}
+        return  0;
     }
 
     public static boolean withinSingleDepositLimit(int amount){
         if(amount >= 100000){
             return false;
         } else{
+            Account.totalDeposit += amount;
             return true;
+        }
+    }
+
+    public static boolean withinSingleWithdrawLimit(int amount){
+        if (Account.mode == true){
+            if(amount > 99999999){
+                return false;
+            }else{
+                Account.totalWithdraw += amount;
+                return true;
+            }
+        }else{
+            if(amount > 100000){
+                return false;
+            }else{
+                Account.totalWithdraw += amount;
+                return true;
+            }
+        }
+    }
+
+    public static boolean withinTotalWithdrawLimit(int amount){
+        if (Account.mode == false && Account.totalWithdraw > 100000){
+            Account.totalWithdraw -= amount;
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public static boolean withinSingleTransferLimit(int amount){
+        if (Account.mode == true){
+            if(amount > 99999999){
+                return false;
+            }else{
+                Account.totalTransfer += amount;
+                return true;
+            }
+        }else{
+            if(amount > 100000){
+                return false;
+            }else{
+                Account.totalTransfer += amount;
+                return true;
+            }
         }
     }
 
@@ -165,138 +281,18 @@ public class BoringBankingSystem {
         return true;
     }
 
-    public static String getStringInput ( String prompt) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String accNumStr;
-        accNumStr = br.readLine();
-        System.out.println(prompt);
-        return accNumStr;
-    }
-
-    public static int getInt( String prompt) throws IOException {
-        try {
-            return Integer.parseInt(getStringInput(prompt));
-        }
-        catch (NumberFormatException e){
-        }
-        return  0;
-    }
-
-    public static void withdraw(){
-        System.out.println("enter account number:");
-        String input = readNextInput();
-        String account_number = null;
-
-        if (validAccList(Integer.parseInt(input))){
-            account_number = input;
-        }
-        else{
-            System.out.println("error: account does not exist, transaction ended");
-            return;
-        }
-
-        System.out.println("enter amount:");
-        input = readNextInput();
-        int amount = 0;
-        try{
-            amount = Integer.parseInt(input);
-        }catch (NumberFormatException e){
-            System.out.println("error: invalid amount, transaction ended");
-            return;
-        }
-
-        double dollars = 0;
-        if (withinSingleWithdrawLimit(amount)){
-            if (withinTotalWithdrawLimit()) {
-                dollars = amount / 100;
-            }
-            else{
-                System.out.println("error: total withdraw limit exceeded");
-                return;
-            }
-        }else{
-            System.out.println("error: single withdraw limit exceeded");
-            return;
-        }
-
-        System.out.printf("Withdrew $%0.2f from account %d", dollars, account_number);
-        Account.summary.add("WDR 0000000 " + amount + " " + account_number + " ***\n");
-
-    }
-
-    public static boolean withinSingleWithdrawLimit(int amount){
-        return true;
-    }
-
-    public static boolean withinTotalWithdrawLimit(){
-        return true;
-    }
-
-    public static void transfer() {
-        System.out.println("transfer from account number:");
-        String input = readNextInput();
-        String account_one, account_two = null;
-
-        if (validAccList(Integer.parseInt(input))){
-            account_one = input;
-        }
-        else{
-            System.out.println("error: account does not exist, transaction ended");
-            return;
-        }
-
-        System.out.println("transfer to account number:");
-        input = readNextInput();
-
-        if (validAccList(Integer.parseInt(input))){
-            account_two = input;
-        }
-        else{
-            System.out.println("error: account does not exist, transaction ended");
-            return;
-        }
-
-        System.out.println("enter amount:");
-        input = readNextInput();
-        int amount = 0;
-        try{
-            amount = Integer.parseInt(input);
-        }catch (NumberFormatException e){
-            System.out.println("error: invalid amount, transaction ended");
-            return;
-        }
-
-        double dollars = 0;
-        if (withinSingleTransferLimit(amount)){
-            dollars = amount / 100;
-        }
-        else{
-            System.out.println("error: single transfer limit exceeded");
-            return;
-        }
-
-        System.out.printf("Transferred $%0.2f from account %d to account %d", dollars, account_one, account_two);
-        Account.summary.add("XFR " + account_one + " " + amount + " " + account_two + " ***\n");
-    }
-
-    public static boolean withinSingleTransferLimit(int amount){
-        return true;
-    }
-
-    public static void Main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception{
         boolean next = true; //used for while loop until the command quit is given
         boolean login = false;
         validAccountsList = readValidAccounts();
-        summaryString = new ArrayList<>() ;// create a summaryString arrayList to store all the summary strings output
         summaryFile = args[1];
         accountsListFile = args[0];
-
 
         while (next){
             if (!login) {
                 Account = waitForLogin(login);
             }
-            String input = readNextInput();
+            String input = getStringInput("");
             switch (input) {
                 case "createacct":
                     createAccount();// append summary string output from create account
--- src/com/company/Session.java
+++ src/com/company/Session.java
@@ -2,8 +2,10 @@ package com.company;
 import java.util.ArrayList;
 
 public class Session {
-    ArrayList<String> summary = new ArrayList<String>();
-    boolean mode; //true is agent & false is machine
+    ArrayList<String> summary = new ArrayList<>();
+    //true is agent & false is machine
+    boolean mode;
+
     int totalTransfer;
     int totalDeposit;
     int totalWithdraw;
